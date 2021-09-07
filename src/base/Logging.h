7598030f433f45d24915dd1a8acc5d621e519a63
HumbleHunger <2495970924@qq.com>
2021-09-07 21:15:23 +0800

    Log

---
+++ src/base/Logging.h
@@ -0,0 +1,26 @@
+#ifndef DJX_LOGGING_H
+#define DJX_LOGGING_H
+
+namespace DJX
+{
+class Logger
+{
+public:
+	enum LogLevel
+	{
+		TRACE,
+    	DEBUG,
+    	INFO,
+    	WARN,
+    	ERROR,
+  		FATAL,
+    	NUM_LOG_LEVELS,
+	};
+private:
+
+};
+
+
+} // namespace DJX
+
+#endif
