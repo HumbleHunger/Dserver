e88b47e128e70ec8b4a6b766e63f6c1780809d33
HumbleHunger <2495970924@qq.com>
2021-09-08 21:02:25 +0800

    Ipml

--- src/base/Logging.cc
+++ src/base/Logging.cc
@@ -38,4 +38,97 @@ class T
   const unsigned len_;
 };
 
+void defaultOutput(const char* msg, int len)
+{
+  size_t n = fwrite(msg, 1, len, stdout);
+  //FIXME check n
+  (void)n;
+}
+
+void defaultFlush()
+{
+  fflush(stdout);
+}
+// 定义Logger的静态成员
+Logger::OutputFunc g_output = defaultOutput;
+Logger::FlushFunc g_flush = defaultFlush;
+
+Logger::Impl::Impl(LogLevel level, int savedErrno, const SourceFile& file, int line)
+  : time_(Timestamp::now()),
+    stream_(),
+    level_(level),
+    line_(line),
+    basename_(file)
+{
+  formatTime();
+  CurrentThread::tid();
+  stream_ << T(CurrentThread::tidString(), CurrentThread::tidStringLength());
+  stream_ << T(LogLevelName[level], 6);
+  if (savedErrno != 0)
+  {
+    stream_ << strerror_tl(savedErrno) << " (errno=" << savedErrno << ") ";
+  }
+}
+
+void Logger::Impl::formatTime()
+{
+  int64_t microSecondsSinceEpoch = time_.microSecondsSinceEpoch();
+  time_t seconds = static_cast<time_t>(microSecondsSinceEpoch / Timestamp::kMicroSecondsPerSecond);
+  int microseconds = static_cast<int>(microSecondsSinceEpoch % Timestamp::kMicroSecondsPerSecond);
+  if (seconds != t_lastSecond)
+  {
+    t_lastSecond = seconds;
+    struct tm tm_time;
+    if (g_logTimeZone.valid())
+    {
+      tm_time = g_logTimeZone.toLocalTime(seconds);
+    }
+    else
+    {
+      ::gmtime_r(&seconds, &tm_time); // FIXME TimeZone::fromUtcTime
+    }
+
+    int len = snprintf(t_time, sizeof(t_time), "%4d%02d%02d %02d:%02d:%02d",
+        tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+        tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
+    assert(len == 17); (void)len;
+  }
+
+  if (g_logTimeZone.valid())
+  {
+    Fmt us(".%06d ", microseconds);
+    assert(us.length() == 8);
+    stream_ << T(t_time, 17) << T(us.data(), 8);
+  }
+  else
+  {
+    Fmt us(".%06dZ ", microseconds);
+    assert(us.length() == 9);
+    stream_ << T(t_time, 17) << T(us.data(), 9);
+  }
+}
+
+void Logger::Impl::finish()
+{
+  stream_ << " - " << basename_ << ':' << line_ << '\n';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 } // namespace DJX
