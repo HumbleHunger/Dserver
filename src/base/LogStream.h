04c33cddbdb63550f1ac61bc483d655b4dfba12d
HumbleHunger <2495970924@qq.com>
2021-09-09 18:34:16 +0800

    FixedBuffer

--- src/base/LogStream.h
+++ src/base/LogStream.h
@@ -1,16 +1,61 @@
 #ifndef DJX_LOGSTREAM_H
 #define	DJX_LOGSTREAM_H
 
+#include "noncopyable.h"
 #include <string.h>
 #include <assert.h>
 
 namespace DJX
 {
+// SIZE是非类型参数
+namespace detail
+{
+template<int SIZE>
+class FixedBuffer : noncopyable
+{
+public:
+	FixedBuffer()
+	: cur_(data_)
+	{}
+	~FixedBuffer() = default;
+	void append(const char* buf, size_t len)
+	{
+		// 如果有足够空间放入缓冲区
+		if (static_cast<size_t>(avail()) > len)
+		{
+			memcpy(cur_, buf, len);
+			// 移动cur_指针
+			cur_ += len;
+		}
+	}
+
+	const char* data() const { return data_; }
+  	int length() const { return static_cast<int>(cur_ - data_); }	
+	char* current() { return cur_; }
+
+	int avail() const { return static_cast<int>(end() - cur_); }
+	void add(size_t len) { cur_ += len; }
+	// 只重置cur_指针
+	void reset() { cur_ = data_; }
+	// 清空缓冲区
+	void bzero() { memZero(data_, sizeof data_); }
+	// 装成String
+	string toString() const { return string(data_, length()); }
+
+private:
+	const char* end() { return data_ + sizeof data_; }
+	
+	char data_[SIZE];
+	char* cur_;
+};
+} // namespace detail
 class LogStream
 {
 public:
 
 private:
+
+
 };
 
 } // namespace DJX
--- src/base/Logging.cc
+++ src/base/Logging.cc
@@ -11,6 +11,15 @@
 
 namespace DJX
 {
+// 线程局部数据储存错误信息
+__thread char t_errnobuf[512];
+__thread char t_time[64];
+__thread time_t t_lastSecond;
+const char* strerror_tl(int savedErrno)
+{
+  return strerror_r(savedErrno, t_errnobuf, sizeof t_errnobuf);
+}
+
 // 定义全局的默认日志等级
 Logger::LogLevel g_logLevel = Logger::INFO;
 
@@ -37,6 +46,18 @@ class T
   const char* str_;
   const unsigned len_;
 };
+// 为自定义类设置的全局的LogStream的operate<<
+inline LogStream& operator<<(LogStream& s, T v)
+{
+  s.append(v.str_, v.len_);
+  return s;
+}
+
+inline LogStream& operator<<(LogStream& s, const Logger::SourceFile& v)
+{
+  s.append(v.data_, v.size_);
+  return s;
+}
 
 void defaultOutput(const char* msg, int len)
 {
@@ -64,6 +85,7 @@ Logger::Impl::Impl(LogLevel level, int savedErrno, const SourceFile& file, int l
   CurrentThread::tid();
   stream_ << T(CurrentThread::tidString(), CurrentThread::tidStringLength());
   stream_ << T(LogLevelName[level], 6);
+  // 如果出现系统错误
   if (savedErrno != 0)
   {
     stream_ << strerror_tl(savedErrno) << " (errno=" << savedErrno << ") ";
@@ -75,48 +97,76 @@ void Logger::Impl::formatTime()
   int64_t microSecondsSinceEpoch = time_.microSecondsSinceEpoch();
   time_t seconds = static_cast<time_t>(microSecondsSinceEpoch / Timestamp::kMicroSecondsPerSecond);
   int microseconds = static_cast<int>(microSecondsSinceEpoch % Timestamp::kMicroSecondsPerSecond);
+  char buf_[32];
   if (seconds != t_lastSecond)
   {
     t_lastSecond = seconds;
     struct tm tm_time;
-    if (g_logTimeZone.valid())
-    {
-      tm_time = g_logTimeZone.toLocalTime(seconds);
-    }
-    else
-    {
-      ::gmtime_r(&seconds, &tm_time); // FIXME TimeZone::fromUtcTime
-    }
+    ::gmtime_r(&seconds, &tm_time); // FIXME TimeZone::fromUtcTime
 
     int len = snprintf(t_time, sizeof(t_time), "%4d%02d%02d %02d:%02d:%02d",
         tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
         tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
     assert(len == 17); (void)len;
   }
-
-  if (g_logTimeZone.valid())
-  {
-    Fmt us(".%06d ", microseconds);
-    assert(us.length() == 8);
-    stream_ << T(t_time, 17) << T(us.data(), 8);
-  }
-  else
-  {
-    Fmt us(".%06dZ ", microseconds);
-    assert(us.length() == 9);
-    stream_ << T(t_time, 17) << T(us.data(), 9);
-  }
+  snprintf(buf_, sizeof buf_, ".%06dZ ", microseconds);
+  stream_ << T(t_time, 17) << T(buf_, 9);
 }
 
 void Logger::Impl::finish()
 {
   stream_ << " - " << basename_ << ':' << line_ << '\n';
 }
+// Logger构造函数
+// INFO日志
+Logger::Logger(SourceFile file, int line)
+  : impl_(INFO, 0, file, line)
+{
+}
+// TARCE,DEBUG
+Logger::Logger(SourceFile file, int line, LogLevel level, const char* func)
+  : impl_(level, 0, file, line)
+{
+    impl_.stream_ << func << ' ';
+}
+// WARN,FATAL,ERROR
+Logger::Logger(SourceFile file, int line, LogLevel level)
+  : impl_(level, 0, file, line)
+{
+}
+// SYS
+Logger::Logger(SourceFile file, int line, bool toAbort)
+  : impl_(toAbort?FATAL:ERROR, errno, file, line)
+{
+}
 
-
-
-
-
+Logger::~Logger()
+{
+  impl_.finish();
+  const LogStream::Buffer& buf(stream().buffer());
+  // 调用输出函数
+  g_output(buf.data(), buf.length());
+  if (impl_.level_ == FATAL)
+  {
+    g_flush();
+    abort();
+  }
+}
+// 设置全局的日志级别
+void Logger::setLogLevel(Logger::LogLevel level)
+{
+  g_logLevel = level;
+}
+// 设置日志输出文件
+void Logger::setOutput(OutputFunc out)
+{
+  g_output = out;
+}
+// 设置刷新流函数
+void Logger::setFlush(FlushFunc flush)
+{
+  g_flush = flush;
+}
 
 
 
