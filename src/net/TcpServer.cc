1f74275f06c3982c58314af9d4aca00ebedd691f
HumbleHunger <2495970924@qq.com>
2021-10-05 20:54:39 +0800

    Tcpserver

---
+++ src/net/TcpServer.cc
@@ -0,0 +1,73 @@
+#include "TcpServer.h"
+
+#include "../base/Logging.h"
+#include "Acceptor.h"
+#include "EventLoop.h"
+#include "EventLoopThreadPool.h"
+#include "SocketOps.h"
+
+using namespace DJX;
+using namespace DJX::net;
+
+TcpServer::TcpServer(const InetAddress& listenAddr)
+	:	listenAddr_(listenAddr),
+		ipPort_(listenAddr.toIpPort()),
+		IOthreadPool_(new EventLoopThreadPool())
+{
+}
+
+TcpServer::~TcpServer()
+{
+	LOG_TRACE << "TcpServer::~TcpServer " << " destructing";
+}
+
+void TcpServer::setThreadNum(int numThreads)
+{
+	assert( 0 <= numThreads);
+	IOthreadPool_->setThreadNum(numThreads);
+}
+
+void TcpServer::start()
+{
+	if (started_.getAndSet(1) == 0)
+	{
+		IOthreadPool_->start();
+	}
+	// 往每个loop插入一个acceptor
+	for ( auto& loop : IOthreadPool_->getAllLoops() )
+	{
+		// 创建Acceptor
+		std::unique_ptr<Acceptor> acceptor(new Acceptor(loop, listenAddr_, true));
+		acceptor->setNewConnectionCallback(std::bind(&TcpServer::newConnection, this, loop, std::placeholders::_1, std::placeholders::_2));
+
+		// 将Acceptor的所有权交给loop
+		loop->setAcceptor(std::move(acceptor));
+	}
+}
+
+void TcpServer::newConnection(EventLoop* loop, int sockfd, const InetAddress& peerAddr)
+{
+	LOG_INFO << "TcpServer::newConnection : EventLoop " <<  loop << " accept new connection from " << peerAddr.toIpPort();
+	InetAddress localAddr(socketOps::getLocalAddr(sockfd));
+	// 新建一个Tcponnection
+	TcpConnectionPtr conn(new TcpConnection(loop,
+	  										sockfd,
+                                        	localAddr,
+                                        	peerAddr));
+	// 设置Tcpconnection的各种回调函数
+	conn->setConnectionCallback(connectionCallback_);
+	conn->setMessageCallback(messageCallback_);
+	conn->setWriteCompleteCallback(writeCompleteCallback_);
+	conn->setCloseCallback(std::bind(&TcpServer::removeConnection, this, std::placeholders::_1));
+	// 将新建的Tcpconnection的channel加入到所属的IO线程loop中的Poller中关注
+	loop->runInLoop(std::bind(&TcpConnection::connectEstablished, conn));
+}
+
+// 被下层Tcpconection::handleclose调用，删除链接
+void TcpServer::removeConnection(const TcpConnectionPtr& conn)
+{
+	LOG_INFO << "TcpServer::removeConnectionInLoop connection ";
+	EventLoop* ioLoop = conn->getLoop();
+	// 将链接和Channel销毁的函数放入loop中的待处理队列
+	ioLoop->queueInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
+}
--- src/net/TcpServer.h
+++ src/net/TcpServer.h
@@ -16,14 +16,43 @@ class Acceptor;
 class EventLoop;
 class EventLoopThreadPool;
 
-// 注册到acceptor中的回调函数，对新链接进行初始化（创建新链接，注册回调函数等）
-void newConnection(int sockfd, const InetAddress& peerAddr);
-
 class TcpServer : noncopyable
 {
 public:
+	TcpServer(const InetAddress& listenAddr);
+	~TcpServer();
+/* 主要接口 */
+	// 设置IO线程池的线程数目
+	void setThreadNum(int numThreads);
+
+	void start();
+
+	// 用户注册的connectioncallback应该处理链接与断开，在TcpConnection链接与断开的时候都会调用
+	void setConnectionCallback(const ConnectionCallback& cb)
+	{ connectionCallback_ = cb; }
+
+	void setMessageCallback(const MessageCallback& cb)
+	{ messageCallback_ = cb; }
+
+	void setWriteCompleteCallback(const WriteCompleteCallback& cb)
+	{ writeCompleteCallback_ = cb; }
+/* 提供私有成员的访问 */
+	// IO线程池
+	std::shared_ptr<EventLoopThreadPool> IOthreadPool()
+	{ return IOthreadPool_; }
+
+	const string& ipPort() const { return ipPort_; }
 
 private:
+	// 注册到acceptor中的回调函数，对新链接进行初始化（创建新链接，注册回调函数等）
+	void newConnection(EventLoop* loop, int sockfd, const InetAddress& peerAddr);
+	// 被下层类TcpConnection回调，处理链接关闭
+	void removeConnection(const TcpConnectionPtr& conn);
+	/// Not thread safe, but in loop
+	void removeConnectionInLoop(const TcpConnectionPtr& conn);
+
+	InetAddress listenAddr_;
+
 	const string ipPort_;
 	std::shared_ptr<EventLoopThreadPool> IOthreadPool_;
 	// 新链接时的回调函数,由用户注册
